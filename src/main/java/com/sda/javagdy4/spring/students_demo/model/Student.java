9fd246788f2e909464da93fafc03b8596e43e6e9
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-11-15 09:13:17 +0100

    student form

--- src/main/java/com/sda/javagdy4/spring/students_demo/model/Student.java
+++ src/main/java/com/sda/javagdy4/spring/students_demo/model/Student.java
@@ -3,6 +3,8 @@ package com.sda.javagdy4.spring.students_demo.model;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.Formula;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Set;
@@ -18,9 +20,11 @@ public class Student {
     private String lastName;
     private boolean graduated;
     private Double homeDistance;
+
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
     private LocalDate birthDate;
     // birthDate jest nazwą kolumny w bazie. Czasami baza nazywa kolumny z podkreślnikami i w tej sytuacji możemy napotkać problemy.
-    @Formula("(year(now())-year(birthDate))")
+    @Formula("(year(now())-year(birth_date))")
     private Integer age;
     @Formula("(SELECT AVG(g.value) FROM grade g where g.student_id = id)")
     private Double gradesAverage;
